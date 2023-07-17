506aff8d97dd5c63c22115acda0b5db4fbfa04a0
gsurica <eosurica@gmail.com>
2023-07-17 20:18:44 -0300

    add: defult launch page

--- restApi/restApi/Controllers/PersonController.cs
+++ restApi/restApi/Controllers/PersonController.cs
@@ -5,7 +5,7 @@ using restApi.Services;
 namespace restApi.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class PersonController : ControllerBase {
 
     private readonly ILogger<PersonController> _logger;
